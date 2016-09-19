44c21e484ea0895b12ee06f870cce37dcc645bc6
Michael Bennett <michael@strukt.org>
2016-09-19 21:54:18 +1000

    Updated code to pull in datatables dependencies for the time being to prove the JS and all the code is working. In time we can push these dependencies into the module most probably.

--- src/angular-datatables.ts
+++ src/angular-datatables.ts
@@ -1,5 +1,8 @@
 import {NgModule} from '@angular/core';
+import {DataTable} from './angular-datatables.directive';
 
 @NgModule({
+    declarations: [DataTable],
+    exports: [DataTable]
 })
 export class DataTablesModule {}
